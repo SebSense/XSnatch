2e62991c3219e190b078ab426700199a864b586b
SebSense <sebastian.senic@gmail.com>
2023-10-18 19:11:40 +0200

    Refactoring and bugfixes Added try-catch tree Fixes crash when file doesn't exist

--- Program.cs
+++ Program.cs
@@ -3,61 +3,50 @@ namespace XSnatch
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             // Define vars:
-            string inputFile, targetName, parentAttribute, parentValue, outputFile;
+            string inputFile, targetName, parentName, parentAttributeKey, parentAttributeValue, outputFile;
 
             //if (args.Length == 0)
             //{
             inputFile = "sma_gentext.xml";
             targetName = "target";
-            parentAttribute = "id";
-            parentValue = "42007";
+            parentName = "*";
+            parentAttributeKey = "id";
+            parentAttributeValue = "42007";
             outputFile = "output.txt";
             //}
-
-
-            /* Verify filetype of inputFile as XML or throw exception
-             * 
-             * INITIALIZE inputType = inputFile.Split('.').Last()
-             * INITIALIZE fileContents AS STRING
-             * DEFINE targetValues AS LIST<STRING>
-             * 
-             * IF inputType = "XML":
-             *      
-             */     //Try to load the file as XML to fileContents or throw exception
-            StreamReader inputStream = new StreamReader(File.Open(inputFile, FileMode.Open));
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(inputStream);
-
-            //Find the element by using XPATH:
-            XmlNode targetNode = xmlDoc.SelectSingleNode($"//*[@{parentAttribute}='{parentValue}']//{targetName}");
-            string result = targetNode.InnerText;
-            Console.WriteLine(result);
-
-            //Placeholders for additional filetype support:
-            /* ELSE IF inputType = "JSON": throw exception
-             * ELSE IF inputType = ... */
-
-            //ELSE: throw exception (invalid file) 
-
-            /* IF targetValues IS EMPTY: PRINT NOT-FOUND-MESSAGE AND RETURN
-             * ELSE: outputString = targetValues.Join(',')
-             * 
-             * IF outputFile EXISTS: Ask to overwrite?
-             *      IF YES: overwrite
-             *      ELSE:   return
-             * ELSE: write outputFile
-             * return
-
-
-            */
-            using (StreamWriter writer = new StreamWriter(outputFile))
+            //NYI: Get the arguments from the command line.
+            try
             {
-                writer.Write(result);
-            }
+                //Open file, search and save match to var
+                string result;
+                using (StreamReader inputStream = new StreamReader(File.Open(inputFile, FileMode.Open)))
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(inputStream);
 
+                    //Find the element by using XPATH:
+                    string xpath = $"//{parentName}[@{parentAttributeKey}='{parentAttributeValue}']//{targetName}";
+                    result = xmlDoc.SelectSingleNode(xpath).InnerText;
+                }
+                //Write to file
+                //TODO: Add prompting for overwrite, rename or quit
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    writer.Write(result);
+                    Console.WriteLine($"Found element <{targetName}> Value: '{result}'");
+                    Console.Write($"Saved to {outputFile}");
+
+                }
+                return 0;
+            }
+            catch (FileNotFoundException ex) // File doesn't exist
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return 1;
+            }
         }
     }
 }
