f8f8d2ae6fd140bdc6e8d8eebfe1d52032cc6337
SebSense <sebastian.senic@gmail.com>
2023-10-18 21:33:31 +0200

    User input functionality done Added custom searching through arguments - see comment for syntax

--- Program.cs
+++ Program.cs
@@ -3,23 +3,114 @@ namespace XSnatch
 {
     internal class Program
     {
+        /* XSnatch arguments:
+         * 
+         * example (win):
+         * XSnatch.exe inputFilePath targetElement parentElement* immediateParent*
+         * 
+         *  * = optional
+         *
+         *      Argument        Example                 Explanation
+         *  inputFilePath   |   filename.xml    |    relative path to an .xml file
+         *  targetElement   |   elem            |    the element whose value you want to extract.
+         *  parentElement   |   elem-attr=val   |    an (optional) parent element. The program will only look for the target value match in this elements child-elements
+         *  immediateParent |   a               |    if included, makes the program only search through the immediate child-elements of the parent. Leave it out for default - searching through all child-elements at any depth.
+         *
+         *  element formatting:
+         *      <elem attr=val>
+         *  where elem is the element you are looking for and attr is an optional attribute with value val
+         *                      
+         *                      Example         |       Match           
+         *                  |   elem            |   matches any "elem" element
+         *                  |   *               |   matches any element
+         *                  |   *-type=34       |   matches any element that has an attribute called type with value 34
+         *                  |   entry-id        |   matches an <entry> element that has an attribute called id with any value
+         * 
+         */
         static int Main(string[] args)
         {
-            // Define vars:
-            string inputFile, targetName, parentName, parentAttributeKey, parentAttributeValue, outputFile;
+            string[] parentElement, targetElement;
+            //Sets the xpath divider chars between parentElement and targetElement to releative.
+            string xpathDivider = "//";
+            string xpath = "//", xpathParent, xpathTarget;
+            char[] trimChars = { '[', ']', ' ' };
+            string arg;
+            string inputFile, outputFile = "output.txt";
 
-            //if (args.Length == 0)
-            //{
-            inputFile = "sma_gentext.xml";
-            targetName = "target";
-            parentName = "*";
-            parentAttributeKey = "id";
-            parentAttributeValue = "42007";
-            outputFile = "output.txt";
-            //}
-            //NYI: Get the arguments from the command line.
             try
             {
+                switch (args.Length)
+                {
+                    case 4:
+                        //Changes the xpath divider between parentElement and targetElement to be absolute, this will only point to immediate children of parentElement
+                        if (args[3] == "a") xpathDivider = "/";
+                        goto case 3;
+                    case 3:
+                        arg = args[2].Trim(trimChars);
+                        parentElement = arg.Split('-');
+                        if (parentElement.Length > 1)
+                        {
+                            xpath += parentElement[0] + "[";
+                            for (int i = 1; i < parentElement.Length; i++)
+                            {
+                                string attr = "@" + parentElement[i];
+                                string key, value;
+                                if (attr.Contains('='))
+                                {
+                                    key = attr.Split("=")[0];
+                                    value = attr.Split("=")[1];
+                                    if (value.First() == '"') value = value.Remove(0, 1);
+                                    if (value.First() != '\'') value = "'" + value;
+                                    if (value.Last() == '"') value = value.Remove(value.Length - 1, 1);
+                                    if (value.Last() != '\'') value = value + "'";
+                                    attr = key + "=" + value;
+                                }
+                                if (i > 1) attr = " and " + attr;
+                                xpath += attr;
+                            }
+                            xpath += "]";
+                        }
+                        else xpath += arg;
+                        xpath += xpathDivider;
+                        goto case 2;
+                    case 2:
+                        arg = args[1].Trim(trimChars);
+                        targetElement = arg.Split('-');
+                        if (targetElement.Length > 1)
+                        {
+                            xpath += targetElement[0] + "[";
+                            Console.WriteLine(xpath);
+                            for (int i = 1; i < targetElement.Length; i++)
+                            {
+                                string attr = "@" + targetElement[i];
+                                string key, value;
+                                if (attr.Contains('='))
+                                {
+                                    key = attr.Split("=")[0];
+                                    value = attr.Split("=")[1];
+                                    if (value.First() != '\'') value = "'" + value;
+                                    if (value.Last() != '\'') value = value + "'";
+                                    attr = key + "=" + value;
+                                }
+                                if (i > 1) attr = " and " + attr;
+                                xpath += attr;
+                            }
+                            xpath += "]";
+                        }
+                        else xpath += arg;
+                        inputFile = args[0];
+                        break;
+                    case 0:
+                        inputFile = "sma_gentext.xml";
+                        outputFile = "output.txt";
+                        xpath = "//*[@id='42007']//target";
+                        break;
+                    default:
+                        throw new ArgumentException("Error: Incorrect number of args. Syntax is: 'XSnatch inputFile targetElement parentElement(optional) absoluteParent(optional)'. See reame.md for more help");
+                }
+                //Check that the file extension is xml
+                if (inputFile.Split('.').Last().ToLower() != "xml")
+                    throw (new FormatException("input file must be an .xml file."));
                 //Open file, search and save match to var
                 string result;
                 using (StreamReader inputStream = new StreamReader(File.Open(inputFile, FileMode.Open)))
@@ -27,8 +118,10 @@ namespace XSnatch
                     XmlDocument xmlDoc = new XmlDocument();
                     xmlDoc.Load(inputStream);
 
+                    //DEBUG: Print the XPATH:
+                    //Console.WriteLine($"Searching with XPATH: \"{xpath}\"");
+
                     //Find the element by using XPATH:
-                    string xpath = $"//{parentName}[@{parentAttributeKey}='{parentAttributeValue}']//{targetName}";
                     result = xmlDoc.SelectSingleNode(xpath).InnerText;
                 }
                 //Write to file
@@ -36,16 +129,31 @@ namespace XSnatch
                 using (StreamWriter writer = new StreamWriter(outputFile))
                 {
                     writer.Write(result);
-                    Console.WriteLine($"Found element <{targetName}> Value: '{result}'");
+                    Console.WriteLine($"Found element! Value: '{result}'");
                     Console.Write($"Saved to {outputFile}");
 
                 }
+                return 0;
+            }
+            catch (NullReferenceException) // No match
+            {
+                Console.WriteLine($"Search complete. No match found.");
                 return 0;
             }
             catch (FileNotFoundException ex) // File doesn't exist
             {
                 Console.WriteLine("Error: " + ex.Message);
                 return 1;
+            }
+            catch (ArgumentException ex) // User tried to input a non .xlm filename
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return 3;
+            }
+            catch (Exception ex) // Unknown error
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return 4;
             }
         }
     }
